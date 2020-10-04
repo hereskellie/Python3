1af83866c3a4dd6a88ffcdd687d4446ea8e3f7d3
Kellie Hughes <kelliemjhughes@gmail.com>
2020-10-04 21:43:53 +0100

    added all files

---
+++ Problems/Factorial of 3/main.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+total == num1 <  num2 < num3
+
+print(total)
---
+++ Problems/Order!/main.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+result == num1 <  num2 < num3
+
+print(result)
+
+take input
+take variable = 2
+in loop(where variable < input )
+print variable
+increase by 2
